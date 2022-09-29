a2088575da8bca7afff8a0e14bb182976a95f144
Lauri <laurih@splunk.com>
2022-09-29 17:14:54 +0000

    demo

--- demo query and update notable.py
+++ demo query and update notable.py
@@ -68,27 +68,7 @@ def run_query_1(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["es100"], callback=filter_1)
-
-    return
-
-
-def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("filter_1() called")
-
-    # collect filtered artifact ids and results for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        logical_operator="or",
-        conditions=[
-            ["run_query_1:action_result.data.*.priority", "==", "high"],
-            ["run_query_1:action_result.data.*.priority", "==", "medium"]
-        ],
-        name="filter_1:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        update_event_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["es100"], callback=update_event_1)
 
     return
 
