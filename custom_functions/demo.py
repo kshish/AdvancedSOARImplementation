eca8e3bc5bbbbf4bbebba2155ab32a651533dc6c
admin <root@localhost>
2022-11-18 17:28:02 +0000

    demo passthrough

---
+++ custom_functions/demo.py
@@ -0,0 +1,25 @@
+def demo(myIp=None, somevalue=None, **kwargs):
+    """
+    demo
+    
+    Args:
+        myIp (CEF type: ip): Please map ip(s) into this parameter
+        somevalue
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        myOutIP (CEF type: ip): Outputs ip(s) as datatype
+        myout
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    myOutIp = myIp
+    myOut = somevalue
+    outpust = {"myOutIp": myOutIp, "myOut": myOut}
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
