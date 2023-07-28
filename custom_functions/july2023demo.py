442daf6e172825d586d48761357be4ac40641872
Chris Huryn <churyn@splunk.com>
2023-07-28 16:28:20 +0000

    demo

---
+++ custom_functions/july2023demo.py
@@ -0,0 +1,26 @@
+def july2023demo(someIP=None, someString=None, **kwargs):
+    """
+    demo
+    
+    Args:
+        someIP (CEF type: ip): Map a IP(s) here
+        someString
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        outputIP (CEF type: ip): This is the resulting IP output from CF
+        outputString
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    outputIP=someIP
+    outputString=someString
+    
+    oputputs = {"outputIP": outputIP, "outputString": someString}
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
