e63f950d36158edb7b88ae44f2807cf2d400494c
soardev <soardev>
2024-03-22 16:23:24 +0000

    d

--- demo http app mar 2024.py
+++ demo http app mar 2024.py
@@ -21,7 +21,7 @@ def on_start(container):
 def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("format_1() called")
 
-    template = """cef?_filter_type={0}\n"""
+    template = """cef?_filter_type=\"{0}\"\n"""
 
     # parameter list for template variable replacement
     parameters = [
