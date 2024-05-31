2af74542a65d1afbaaa9feb598c71a4279d97550
soardev <soardev>
2024-05-31 18:44:27 +0000

    demo http

--- demo http app.py
+++ demo http app.py
@@ -12,53 +12,22 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'prompt_1' block
-    prompt_1(container=container)
-
-    return
-
-@phantom.playbook_block()
-def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("format_1() called")
-
-    template = """cef?filter_type={0}\n"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "prompt_1:action_result.summary.responses.0"
-    ]
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
-
+    # call 'get_data_1' block
     get_data_1(container=container)
 
     return
 
-
 @phantom.playbook_block()
-def get_data_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+def get_data_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("get_data_1() called")
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
-    format_1 = phantom.get_format_data(name="format_1")
-
     parameters = []
 
-    if format_1 is not None:
-        parameters.append({
-            "location": format_1,
-        })
+    parameters.append({
+        "location": "ph_user",
+    })
 
     ################################################################################
     ## Custom Code Start
@@ -70,39 +39,7 @@ def get_data_1(action=None, success=None, container=None, results=None, handle=N
     ## Custom Code End
     ################################################################################
 
-    phantom.act("get data", parameters=parameters, name="get_data_1", assets=["soar100"])
-
-    return
-
-
-@phantom.playbook_block()
-def prompt_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("prompt_1() called")
-
-    # set user and message variables for phantom.prompt call
-
-    user = None
-    role = "Administrator"
-    message = """"""
-
-    # parameter list for template variable replacement
-    parameters = []
-
-    # responses
-    response_types = [
-        {
-            "prompt": "What type of CEF fields would you like to see",
-            "options": {
-                "type": "list",
-                "choices": [
-                    "custom",
-                    "default"
-                ],
-            },
-        }
-    ]
-
-    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types, callback=format_1)
+    phantom.act("get data", parameters=parameters, name="get_data_1", assets=["my soar instance"])
 
     return
 
