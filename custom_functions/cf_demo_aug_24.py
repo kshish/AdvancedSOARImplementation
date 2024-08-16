ad38ce7cb0b2efd4ae13a5c0384f7a251931b6d4
soardev <soardev>
2024-08-16 01:42:48 +0000

    demo passthrough ip and string cf

---
+++ custom_functions/cf_demo_aug_24.py
@@ -0,0 +1,28 @@
+def cf_demo_aug_24(someIP=None, someString=None, **kwargs):
+    """
+    demo
+    
+    Args:
+        someIP (CEF type: ip): provide an IP or list of IPs
+        someString
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        outputIP (CEF type: ip): this is output IP from demo cf
+        outputString
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    
+    outputIP=someIP
+    outputString=someString
+    
+    
+    outputs = {"outputIP" : outputIP   , "outputsString": outputString   }
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
