2c602826d5679740b45018d65efc1fcd4f9379a4
soardev <soardev>
2025-01-07 20:28:44 +0000

    add comment to container

--- jan 2025 run saved search demo.py
+++ jan 2025 run saved search demo.py
@@ -72,7 +72,58 @@ def run_query_1(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["instructor soar"])
+    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["instructor soar"], callback=format_2)
+
+    return
+
+
+@phantom.playbook_block()
+def format_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_2() called")
+
+    template = """Peer: {0} with {2} priority communicated {1} times\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "run_query_1:action_result.data.*.peer",
+        "run_query_1:action_result.data.*.count",
+        "run_query_1:action_result.data.*.priority"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_2")
+
+    add_comment_1(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_comment_1() called")
+
+    format_2 = phantom.get_format_data(name="format_2")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment=format_2)
 
     return
 
