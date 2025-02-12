5bc4ee7a908697a85281abddcfd773b864f22347
soardev <soardev>
2025-02-12 18:47:20 +0000

    simple CF example

---
+++ custom_functions/passthrough_2025_feb.py
@@ -0,0 +1,26 @@
+def passthrough_2025_feb(Ip_input=None, someString_input=None, **kwargs):
+    """
+    this will pass values through input to the output
+    
+    Args:
+        Ip_input (CEF type: ip): map an IP address into this input
+        someString_input
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        Ip_output (CEF type: ip): this is the same ip as was passed in
+        string_output
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+ 
+    # Write your custom code here...
+    some_ip=Ip_input
+    some_string=someString_input
+    
+    ouputs={"Ip_output": some_ip, "string_output": some_string}
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
