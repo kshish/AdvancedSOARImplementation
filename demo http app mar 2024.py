7c9cd80d32eed40e786a04871435e912be3498b6
soardev <soardev>
2025-03-13 19:36:41 +0000

    resolve asset name

--- demo http app mar 2024.py
+++ demo http app mar 2024.py
@@ -70,7 +70,7 @@ def get_data_1(action=None, success=None, container=None, results=None, handle=N
     ## Custom Code End
     ################################################################################
 
-    phantom.act("get data", parameters=parameters, name="get_data_1", assets=["myownsoar"], callback=format_2)
+    phantom.act("get data", parameters=parameters, name="get_data_1", assets=["my instructor soar"], callback=format_2)
 
     return
 
