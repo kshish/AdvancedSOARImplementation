0ec352e06378bf31a514d27dbfe720a22f18dc27
Dilip Muralidaran <dmuralidaran@splunk.com>
2025-03-20 04:41:42 +0000

    demo use of query results

--- similar to lab 4 demo.py
+++ similar to lab 4 demo.py
@@ -73,7 +73,116 @@ def run_query_1(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["instructorsplunkes"])
+    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["instructorsplunkes"], callback=format_preamble)
+
+    return
+
+
+@phantom.playbook_block()
+def format_preamble(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_preamble() called")
+
+    template = """Host {0} communicated with {1} other servers. Here is a list of the other servers:\n\n\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "artifact:*.cef.destination",
+        "run_query_1:action_result.summary.total_events"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_preamble")
+
+    format_query_results(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def format_query_results(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_query_results() called")
+
+    template = """%%\npeer: {0} with severity: {1}, {2} times\n%%"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "run_query_1:action_result.data.*.peer",
+        "run_query_1:action_result.data.*.priority",
+        "run_query_1:action_result.data.*.count"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_query_results")
+
+    format_whole_msg(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def format_whole_msg(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_whole_msg() called")
+
+    template = """{0}\n{1}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "format_preamble:formatted_data",
+        "format_query_results:formatted_data"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_whole_msg")
+
+    add_comment_2(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_comment_2() called")
+
+    format_whole_msg = phantom.get_format_data(name="format_whole_msg")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment=format_whole_msg)
 
     return
 
