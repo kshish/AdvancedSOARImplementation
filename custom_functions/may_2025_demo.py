50f47655249016e938841eb02025dfe07c4d38be
soardev <soardev>
2025-05-14 17:14:07 +0000

    custom passthrough demo

---
+++ custom_functions/may_2025_demo.py
@@ -0,0 +1,38 @@
+def may_2025_demo(myDestination=None, myPriority=None, **kwargs):
+    """
+    simple passthrough demo
+    
+    Args:
+        myDestination (CEF type: ip): map some ip into this input
+        myPriority: map a priority that will get increased to next level
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        outputDestination (CEF type: ip)
+        newPriority: one level higher
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    
+    tempOutDest = myDestination + " validated"
+    tempOutPriority = "critical"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    outputs = {"outputDestination": tempOutDest , "newPriority": tempOutPriority}
+    
+    # Write your custom code here...
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
