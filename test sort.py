a9b691e3e4887286452ee469940e60782afd2354
soardev <soardev>
2026-02-12 18:57:14 +0000

    test

--- test sort.py
+++ test sort.py
@@ -12,24 +12,23 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'sort_list_3' block
-    sort_list_3(container=container)
+    # call 'sort_list_4' block
+    sort_list_4(container=container)
 
     return
 
 @phantom.playbook_block()
-def sort_list_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("sort_list_3() called")
+def sort_list_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("sort_list_4() called")
 
     container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.destinationAddress","artifact:*.id"])
 
     parameters = []
 
-    # build parameters list for 'sort_list_3' call
+    # build parameters list for 'sort_list_4' call
     for container_artifact_item in container_artifact_data:
         parameters.append({
             "unsorted_list": container_artifact_item[0],
-            "sort_order": "a",
         })
 
     ################################################################################
@@ -42,28 +41,28 @@ def sort_list_3(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="asi/sort_list", parameters=parameters, name="sort_list_3", callback=prompt_2)
+    phantom.custom_function(custom_function="asi/sort_list", parameters=parameters, name="sort_list_4", callback=prompt_3)
 
     return
 
 
 @phantom.playbook_block()
-def prompt_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("prompt_2() called")
+def prompt_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("prompt_3() called")
 
     # set approver and message variables for phantom.prompt call
 
     user = None
     role = "Administrator"
-    message = """Here's the unsorted list:\n\n{0}\n\nHere's the sorted list: \n\n{1}"""
+    message = """Unsorted:\n\n{0}\n\nSorted:\n\n{1}"""
 
     # parameter list for template variable replacement
     parameters = [
         "artifact:*.cef.destinationAddress",
-        "sort_list_3:custom_function_result.data.sorted_list"
+        "sort_list_4:custom_function_result.data.sorted_list"
     ]
 
-    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="prompt_2", parameters=parameters)
+    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="prompt_3", parameters=parameters)
 
     return
 
